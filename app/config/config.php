415eef609e51c5ac39db78f806f0f5a204e68061
EwilsonS <ewilsons@aol.com>
2018-11-27 11:30:42 -0500

    core mvc frame

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
     //DB Params
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'kalamazoo123'); //check!!!!!!!!!!!!
-    define('DB_NAME', 'wmvc');
+    define('DB_USER', '_Your_info');
+    define('DB_PASS', '_Your_info');
+    define('DB_NAME', '_Your_info');
     
     //App Root
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URL Root
-    define('URLROOT','http://localhost/wilsonmvcphp');
+    define('URLROOT','http://localhost/_your_url');
     
     //site name
-    define('SITENAME', 'WilsonMVC');
+    define('SITENAME', 'Your_sitename');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,15 +1,13 @@
 <?php
     class Pages extends Controller {
         public function __construct(){
-            $this->postModel = $this->model('Post');
+
         }
 
         public function index(){
-            $posts = $this->postModel->getPosts();
 
             $data = [
                 'title' => 'Welcome',
-                'posts' => $posts
             ];
 
             $this->view('pages/index', $data);
