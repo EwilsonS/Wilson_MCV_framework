ed003131d4fcd96824f05f542e19d029d4f7f1c0
EwilsonS <ewilsons@aol.com>
2018-11-27 11:31:56 -0500

    core mvc frame

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1><?php echo  $data['title']; ?></h1>
+<p>This is the Wilson MVC framework, please refer to the documentation</p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
